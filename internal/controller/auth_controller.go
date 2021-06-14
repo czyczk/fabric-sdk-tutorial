cd6fbacf143c310238480826fc3eddcef34b9385
Zenas Chen <czyczk@qq.com>
2021-06-15 01:18:59 +0800

    Add: AuthController: Get auth sessions

--- internal/controller/auth_controller.go
+++ internal/controller/auth_controller.go
@@ -25,25 +25,26 @@ func (c *AuthController) GetEndpointMap() EndpointMap {
 	return EndpointMap{
 		urlMethodPair{"request", "POST"}:  []gin.HandlerFunc{c.handleCreateAuthRequest},
 		urlMethodPair{"response", "POST"}: []gin.HandlerFunc{c.handleCreateAuthResponse},
+		urlMethodPair{":id", "GET"}:       []gin.HandlerFunc{c.handleGetAuthSession},
 	}
 }
 
-func (ac *AuthController) handleCreateAuthRequest(c *gin.Context) {
+func (c *AuthController) handleCreateAuthRequest(ctx *gin.Context) {
 	// Validity check
 	pel := &ParameterErrorList{}
 
-	resourceID := c.PostForm("resourceID")
+	resourceID := ctx.PostForm("resourceID")
 	resourceID = pel.AppendIfEmptyOrBlankSpaces(resourceID, "资源 ID 不能为空。")
 
-	reason := c.PostForm("reason")
+	reason := ctx.PostForm("reason")
 
 	// Early return after extracting common parameters if the error list is not empty
 	if len(*pel) > 0 {
-		c.AbortWithStatusJSON(http.StatusBadRequest, pel)
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, pel)
 		return
 	}
 
-	txID, err := ac.AuthSvc.CreateAuthRequest(resourceID, reason)
+	txID, err := c.AuthSvc.CreateAuthRequest(resourceID, reason)
 
 	// Check error type and generate the corresponding response
 	// The symmetric key will be included if it's not empty
@@ -51,44 +52,67 @@ func (ac *AuthController) handleCreateAuthRequest(c *gin.Context) {
 		info := TransactionIDInfo{
 			TransactionID: txID,
 		}
-		c.JSON(http.StatusOK, info)
+		ctx.JSON(http.StatusOK, info)
 	} else if errors.Cause(err) == errorcode.ErrorNotImplemented {
-		c.Writer.WriteHeader(http.StatusNotImplemented)
+		ctx.Writer.WriteHeader(http.StatusNotImplemented)
 	} else {
-		c.String(http.StatusInternalServerError, err.Error())
+		ctx.String(http.StatusInternalServerError, err.Error())
 	}
 }
 
-func (ac *AuthController) handleCreateAuthResponse(c *gin.Context) {
+func (c *AuthController) handleCreateAuthResponse(ctx *gin.Context) {
 	// Validity check
 	pel := &ParameterErrorList{}
 
-	authSessionID := c.PostForm("authSessionID")
+	authSessionID := ctx.PostForm("authSessionID")
 	authSessionID = pel.AppendIfEmptyOrBlankSpaces(authSessionID, "授权会话 ID 不能为空。")
 
-	result := c.PostForm("result")
+	result := ctx.PostForm("result")
 	result = pel.AppendIfEmptyOrBlankSpaces(result, "授权结果不能为空。")
 	resultBool := pel.AppendIfNotBool(result, "授权结果须为 bool 值。")
 
 	// Early return after extracting common parameters if the error list is not empty
 	if len(*pel) > 0 {
-		c.AbortWithStatusJSON(http.StatusBadRequest, pel)
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, pel)
 		return
 	}
 
-	txID, err := ac.AuthSvc.CreateAuthResponse(authSessionID, resultBool)
+	txID, err := c.AuthSvc.CreateAuthResponse(authSessionID, resultBool)
 	// Check error type and generate the corresponding response
 	// The symmetric key will be included if it's not empty
 	if err == nil {
 		info := TransactionIDInfo{
 			TransactionID: txID,
 		}
-		c.JSON(http.StatusOK, info)
+		ctx.JSON(http.StatusOK, info)
 	} else if errors.Cause(err) == errorcode.ErrorNotImplemented {
-		c.Writer.WriteHeader(http.StatusNotImplemented)
+		ctx.Writer.WriteHeader(http.StatusNotImplemented)
 	} else if errors.Cause(err) == errorcode.ErrorForbidden {
-		c.Writer.WriteHeader(http.StatusForbidden)
+		ctx.Writer.WriteHeader(http.StatusForbidden)
 	} else {
-		c.String(http.StatusInternalServerError, err.Error())
+		ctx.String(http.StatusInternalServerError, err.Error())
+	}
+}
+
+func (c *AuthController) handleGetAuthSession(ctx *gin.Context) {
+	// Validity check
+	pel := &ParameterErrorList{}
+	id := pel.AppendIfEmptyOrBlankSpaces(ctx.Param("id"), "授权会话 ID 不能为空")
+
+	// Early return if there's parameter error
+	if len(*pel) != 0 {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, *pel)
+		return
+	}
+
+	authSession, err := c.AuthSvc.GetAuthSession(id)
+	if err == nil {
+		ctx.JSON(http.StatusOK, authSession)
+	} else if errors.Cause(err) == errorcode.ErrorNotFound {
+		ctx.Writer.WriteHeader(http.StatusNotFound)
+	} else if errors.Cause(err) == errorcode.ErrorNotImplemented {
+		ctx.Writer.WriteHeader(http.StatusNotImplemented)
+	} else {
+		ctx.String(http.StatusInternalServerError, err.Error())
 	}
 }
